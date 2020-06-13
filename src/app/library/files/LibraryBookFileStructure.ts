caf44c1e6a4067763ba5979aa4784d24070d488f
Geoffrey <dotrey@gmail.com>
2020-06-13 20:55:11 +0200

    updated words to be able to consist of multiple chars

--- src/app/library/files/LibraryBookFileStructure.ts
+++ src/app/library/files/LibraryBookFileStructure.ts
@@ -15,10 +15,11 @@ export default interface LibraryBookFileStructure {
      */
     words : {
         /**
-         * The id of the word, which is the unicode in hex of the symbol. If the
-         * unicode has less than 5 characters, it will be left-padded with 0.
+         * The ids of the characters forming the word, which are the unicodes 
+         * in hex of the symbols. If the unicode has less than 5 characters, it
+         * will be left-padded with 0.
          */
-        id : string;
+        id : string[];
 
         /**
          * The romaji (transcription with latin characters) of the word. There can
