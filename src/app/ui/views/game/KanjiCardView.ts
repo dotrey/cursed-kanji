35637de1abad7af34f0d5f31b912e79750390ac9
Geoffrey <dotrey@gmail.com>
2020-07-08 21:40:00 +0200

    Show correct romaji if kanji isn't solved within timelimit

--- src/app/ui/views/game/KanjiCardView.ts
+++ src/app/ui/views/game/KanjiCardView.ts
@@ -9,26 +9,38 @@ const KanjiCardView = {
         if (seconds.indexOf(".") < 0) {
             seconds += ".0";
         }
-        return m(".kanjicard.phase-" + phase, {            
-            onclick : () => {
-                window.location.hash = "#!/game/detail";
-            }
-        }, [
-            m(".kanjicard-word", {
-                onupdate : function(vnode : any) {
-                    if (vnode.dom.innerText !== KanjiCardView.lastKanji) {
-                        let scale : number = 1;
-                        if (vnode.dom.offsetWidth > vnode.dom.parentNode.offsetWidth) {
-                            scale = vnode.dom.parentNode.offsetWidth / vnode.dom.offsetWidth;
-                        }
-                        vnode.dom.setAttribute("style", "--scale:" + scale);
-                        KanjiCardView.lastKanji = vnode.dom.innerText;
-                    }
+        return [
+            m(".kanjicard.phase-" + phase, {            
+                onclick : () => {
+                    window.location.hash = "#!/game/detail";
                 }
-            }, m.trust(vnode.attrs.game.status.kanji)),
-            m(".kanjicard-timer", seconds),
-            m(".kanjicard-details", "tap for details")
-        ]);
+            }, [
+                m(".kanjicard-word", {
+                    onupdate : function(vnode : any) {
+                        if (vnode.dom.innerText !== KanjiCardView.lastKanji) {
+                            let scale : number = 1;
+                            if (vnode.dom.offsetWidth > vnode.dom.parentNode.offsetWidth) {
+                                scale = vnode.dom.parentNode.offsetWidth / vnode.dom.offsetWidth;
+                            }
+                            vnode.dom.setAttribute("style", "--scale:" + scale);
+                            KanjiCardView.lastKanji = vnode.dom.innerText;
+                        }
+                    }
+                }, m.trust(vnode.attrs.game.status.kanji)),
+                m(".kanjicard-timer", seconds),
+                m(".kanjicard-details", "tap for details"),
+            ]),
+            m(".kanjicard-solution.phase-" + phase, [
+                vnode.attrs.game.status.word ?
+                m.trust(vnode.attrs.game.status.word.romaji.reduce((a : string, c : string) => {
+                    if (a) {
+                        a += "<br>";
+                    }
+                    return a + c;
+                }), "") :
+                ""
+            ])
+        ];
     }
 };
 
